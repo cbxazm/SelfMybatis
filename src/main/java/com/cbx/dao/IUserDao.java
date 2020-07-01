d2533a94a54ef9edc9b77680183ed745b7a1cf02
cbxa <895001956@qq.com>
2020-07-01 09:33:39 +0800

    基于注解的方法

--- src/main/java/com/cbx/dao/IUserDao.java
+++ src/main/java/com/cbx/dao/IUserDao.java
@@ -1,9 +1,11 @@
 package com.cbx.dao;
 
+import com.cbx.annotation.Select;
 import com.cbx.pojo.User;
 
 import java.util.List;
 
 public interface IUserDao {
+    @Select("select * from user")
     List<User> findAll();
 }
